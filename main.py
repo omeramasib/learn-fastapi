24f6092a931854877130f891409bfd4eb7352f15
omeramasib <omeramasib999@gmail.com>
2024-11-18 11:37:30 +0300

    added new changes

--- main.py
+++ main.py
@@ -3,5 +3,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def blog():
+    return {"data": "blog list"}
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished blogs"}
+
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id: int):
+    return {"data": id}
