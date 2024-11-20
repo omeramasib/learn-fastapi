99862e24273d426cd183e8e0584e538b5a04a5e6
omeramasib <omeramasib999@gmail.com>
2024-11-20 16:18:25 +0300

    add class and query parameters

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
-
+from typing import Optional
+from pydantic import BaseModel
 app = FastAPI()
 
-@app.get("/")
-def blog():
-    return {"data": "blog list"}
+@app.get("/blog")
+def blog(limit, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {"data": f"{limit} published blogs"}
+    else:
+        return {"data": f"{limit} blogs from db"}
 
 @app.get("/blog/unpublished")
 def unpublished():
@@ -18,3 +22,14 @@ def show(id: int):
 @app.get("/blog/{id}/comments")
 def comments(id: int):
     return {"data": id}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post("/blog")
+def blog_create(request: Blog):
+    print(request)
+    return request
+    # return {"data": "blog is created"}
