a1314b381c595c5c64a39ef0cc77ce039393b217
omeramasib <omeramasib999@gmail.com>
2024-12-03 15:49:27 +0300

    add my latest changes

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,20 @@
+from typing import List
 from fastapi import FastAPI, Depends, Response, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 from . import schemas, models
 from . database import engine, SessionLocal
 from sqlalchemy.orm import Session
+
 app = FastAPI()
+
+# Configure CORS with more permissive settings for development
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins in development
+    allow_credentials=False,  # Must be False when allow_origins=["*"]
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 models.Base.metadata.create_all(engine)
 
@@ -12,6 +24,11 @@ def get_db():
         yield db
     finally:
         db.close()
+
+@app.get('/blogs', response_model= List[schemas.ShowBlog])
+def get_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
 
 @app.post('/blog', status_code= status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
@@ -27,7 +44,7 @@ def delete(id, db: Session = Depends(get_db)):
     db.commit()
     return {"message":"Blog Deleted Successfully"}
 
-@app.get('/blog/{id}', status_code= status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code= status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if(not blog):
@@ -36,16 +53,64 @@ def show(id, response: Response, db: Session = Depends(get_db)):
 
 @app.put('/blog/{id}', status_code= status.HTTP_202_ACCEPTED)
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).update({'title': request.title, 'body': request.body})
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} is not available")
+    blog.update(request.dict())
     db.commit()
     return {"message":"Updated Successfully"}
 
+# from typing import List
+# from fastapi import FastAPI, Depends, Response, HTTPException, status
+# from . import schemas, models
+# from . database import engine, SessionLocal
+# from sqlalchemy.orm import Session
+# app = FastAPI()
 
+# models.Base.metadata.create_all(engine)
 
-@app.get('/blog')
-def get_blogs(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return  blogs
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+# @app.get('/blogs', response_model= List[schemas.ShowBlog])
+# def get_blogs(db: Session = Depends(get_db)):
+#     blogs = db.query(models.Blog).all()
+#     return  blogs
+
+# @app.post('/blog', status_code= status.HTTP_201_CREATED)
+# def create(request: schemas.Blog, db: Session = Depends(get_db)):
+#     new_blog = models.Blog(title=request.title, body=request.body)
+#     db.add(new_blog)
+#     db.commit()
+#     db.refresh(new_blog)
+#     return new_blog
+
+# @app.delete('/blog/{id}', status_code= status.HTTP_204_NO_CONTENT)
+# def delete(id, db: Session = Depends(get_db)):
+#     db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+#     db.commit()
+#     return {"message":"Blog Deleted Successfully"}
+
+# @app.get('/blog/{id}', status_code= status.HTTP_200_OK, response_model=schemas.ShowBlog)
+# def show(id, response: Response, db: Session = Depends(get_db)):
+#     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+#     if(not blog):
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} is not available")
+#     return blog
+
+# @app.put('/blog/{id}', status_code= status.HTTP_202_ACCEPTED)
+# def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+#     blog = db.query(models.Blog).filter(models.Blog.id == id)
+#     if not blog.first():
+#      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} is not available")
+#     blog.update(request.dict())
+#     db.commit()
+#     return {"message":"Updated Successfully"}
+    
 
 
 
